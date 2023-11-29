e7b34a3b61664bd7135b1d00f76b7d5c15c82b00
ir4kgl <vishneviyshturman@gmail.com>
2023-11-29 19:28:02 +0300

    minor fix

--- vocoder/loss/FMLoss.py
+++ vocoder/loss/FMLoss.py
@@ -5,6 +5,7 @@ class FMLoss():
     def __call__(self, maps_gt, maps_pred):
         fm_loss = 0.
         for i in range(len(maps_gt)):
-            fm_loss += torch.mean(
-                torch.stack(maps_gt[i]) - torch.stack(maps_pred[i]))
+            for j in range(len(maps_gt[i])):
+                fm_loss += torch.mean(
+                    torch.abs(maps_gt[i][j]- maps_pred[i][j]))
         return fm_loss
