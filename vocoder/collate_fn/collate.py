7f4c76e709a82419c946404a13b2a217c5f1e468
ir4kgl <vishneviyshturman@gmail.com>
2023-11-30 10:44:41 +0300

    minor fix

--- vocoder/collate_fn/collate.py
+++ vocoder/collate_fn/collate.py
@@ -3,6 +3,7 @@ from typing import List
 
 import torch
 from torch.nn import ConstantPad2d
+from torch.nn.functional import pad
 
 logger = logging.getLogger(__name__)
 
@@ -29,11 +30,11 @@ def collate_fn(dataset_items: List[dict]):
     batch_mel_len = max(result_batch["mel_len"])
 
     result_batch["audio"] = torch.cat(
-        tuple(ConstantPad2d((0, batch_audio_len - x["audio"].shape[-1], 0, 0), 0)(x["audio"]) for x in dataset_items)
+        tuple(pad(x["audio"], (0, batch_audio_len - x["audio"].shape[-1]), "constant") for x in dataset_items)
     ).unsqueeze(1)
 
     result_batch["mel"] = torch.cat(
-        tuple(ConstantPad2d((0, batch_mel_len - x["mel"].shape[-1], 0, 0), 0)(x["mel"]) for x in dataset_items)
+        tuple(pad(x["mel"], (0, batch_mel_len - x["mel"].shape[-1]), "constant") for x in dataset_items)
     )
 
     return result_batch
