e4ca281481f87e941725fd5974d92e689457ca6e
ir4kgl <vishneviyshturman@gmail.com>
2023-12-04 01:09:50 +0300

    add checkpoint link

--- download_checkpoint.py
+++ download_checkpoint.py
@@ -2,5 +2,6 @@ import gdown
 
 checkpoint_dir = "./checkpoints/"
 
-gdown.download("?",
+gdown.download("https://drive.google.com/uc?id=1iw_Ak2-vuKCbAN4PnwRPeW6T-mJ0acPg",
                checkpoint_dir + "final_run/" + "checkpoint.pth", quiet=True)
+
